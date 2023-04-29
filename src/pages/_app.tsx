2b0d890dc8883af0af180be8d14ca72932f6e936
George Lewis <ghl227@gmail.com>
2023-04-29 14:46:12 -0400

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,12 @@ import "semantic-ui-css/semantic.min.css";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
       <Component {...pageProps} />
+      <Analytics />
     </React.StrictMode>
   );
 }
