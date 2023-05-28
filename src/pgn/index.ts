5192a502596ed908ffcc31940409920214173e26
George Lewis <ghl227@gmail.com>
2023-05-28 14:11:08 -0400

    Remove unnecessary code

--- src/pgn/index.ts
+++ src/pgn/index.ts
@@ -1,11 +1,5 @@
 import pegjs from "peggy";
-
 import grammar from "./grammar.pegjs";
-
-//import fs from "fs";
-//import { path as rootPath } from "app-root-path";
-
-//const grammar = fs.readFileSync(`${rootPath}/src/pgn/grammar.pegjs`, "utf-8");
 
 const parser = pegjs.generate(grammar);
 
